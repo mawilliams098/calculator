56d450f1b937407a945e120ad1e581bea5d5dc5f
mawilliams098 <mawilliams098@gmail.com>
2024-03-19 20:41:12 -0400

    Add basic arithmetic functions

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log("hi");
+let firstNum;
+let secondNum;
+let operator;
+let displayValue;
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, firstNum, secondNum) {
+    if (operator == "+") {
+        return add(firstNum, secondNum);
+    } else if (operator == "-") {
+        return subtract(firstNum, secondNum);
+    } else if (operator == "*") {
+        return multiply(firstNum, secondNum);
+    } else if (operator == "/") {
+        return divide(firstNum, secondNum);
+    }
+}
