249dddef9d3b98e29cded765366ecd85d4d326d6
mawilliams098 <mawilliams098@gmail.com>
2024-03-20 15:25:46 -0400

    Setup basic frontend calculator and buttons

--- script.js
+++ script.js
@@ -3,6 +3,22 @@ let secondNum;
 let operator;
 let displayValue;
 
+buttonContainer = document.querySelector("#button-container")
+// There are 6 rows of buttons on the TI-108, each with 4 buttons
+for (let i = 0; i < 6; i++) {
+    let row = document.createElement("div")
+    buttonContainer.appendChild(row);
+    row.setAttribute("class", "row");
+    for (let j = 0; j < 4; j++) {
+        let button = document.createElement("button");
+        if (j === 3) {
+            button.setAttribute("class", "right-button");
+        } else {
+            button.setAttribute("class", "left-button");
+        }
+        row.appendChild(button);
+    }
+}
 
 function add(a, b) {
     return a + b;
