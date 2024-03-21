5463d96d97431b16eb63b3a67ad3455cc9378931
mawilliams098 <mawilliams098@gmail.com>
2024-03-20 21:39:47 -0400

    Implement basic two number operations

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
-let firstNum;
-let secondNum;
+let firstNum = "";
+let secondNum = "";
 let operator;
 let displayValue = "";
 
 const buttonText = ["+/-", "sqrt", "%", "div", "MRC", "M-", "M+", "X",
                 "7", "8", "9", "-", "4", "5", "6", "+", "1", "2", "3", "=",
                 "ON/C", "0", ".", "blank"];
-
-let screen = document.querySelector("#screen");
-window.addEventListener("click", (event) => {
-    let buttonName = event.target.id;
-    if (buttonName >= "0" && buttonName <= "9") {
-        displayValue += buttonName;
-    } 
-    screen.textContent = displayValue;
-})
 
 buttonContainer = document.querySelector("#button-container")
 // There are 6 rows of buttons on the TI-108, each with 4 buttons
@@ -38,6 +29,36 @@ for (let i = 0; i < buttons.length; i++) {
     buttons[i].textContent = buttonText[i];
     buttons[i].setAttribute("id", buttonText[i]);
 }
+
+let screen = document.querySelector("#screen");
+window.addEventListener("click", (event) => {
+    let buttonName = event.target.id;
+    if (buttonName >= "0" && buttonName <= "9") {
+        if (firstNum !== "") {
+            secondNum += buttonName;
+            displayValue = secondNum;
+        } else {
+            displayValue += buttonName;
+        }
+        screen.textContent = displayValue;
+    } else if (buttonName === "+") {
+        firstNum = displayValue;
+        operator = buttonName;
+    } else if (buttonName === "-") {
+        firstNum = displayValue;
+        operator = buttonName;
+    } else if (buttonName === "*") {
+        firstNum = displayValue;
+        operator = buttonName;
+    } else if (buttonName === "/") {
+        firstNum = displayValue;
+        operator = buttonName;
+    } else if (buttonName === "=") {
+        // screen.textContent = '';
+        console.log(operate(operator, +firstNum, +secondNum));
+        screen.textContent = operate(operator, +firstNum, +secondNum);
+    }
+})
 
 function add(a, b) {
     return a + b;
