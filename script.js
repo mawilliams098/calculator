521a0ad2a52c537203c945f57323d72bf3ed1463
mawilliams098 <mawilliams098@gmail.com>
2024-03-21 22:18:34 -0400

    Clean up operator cases and allow chaining operations

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let firstNum = "";
 let secondNum = "";
-let operator;
 let displayValue = "";
+let operator = "";
 
 const buttonText = ["+/-", "sqrt", "%", "div", "MRC", "M-", "M+", "X",
                 "7", "8", "9", "-", "4", "5", "6", "+", "1", "2", "3", "=",
                 "ON/C", "0", ".", "blank"];
+const operators = ["X", "-", "+", "/"]
 
 buttonContainer = document.querySelector("#button-container")
 // There are 6 rows of buttons on the TI-108, each with 4 buttons
@@ -32,31 +33,35 @@ for (let i = 0; i < buttons.length; i++) {
 
 let screen = document.querySelector("#screen");
 window.addEventListener("click", (event) => {
-    let buttonName = event.target.id;
+    let buttonName = event.target.id;  
     if (buttonName >= "0" && buttonName <= "9") {
-        if (firstNum !== "") {
-            secondNum += buttonName;
-            displayValue = secondNum;
-        } else {
-            displayValue += buttonName;
-        }
+        displayValue += buttonName;
         screen.textContent = displayValue;
-    } else if (buttonName === "+") {
-        firstNum = displayValue;
+    } else if (operators.includes(buttonName)) {
+        // If this is an operator stacked on top of a preceding operator
+        if (operator !== "") {
+            // Save the second number 
+            secondNum = displayValue;
+            // Calculate the result of the first operation 
+            let res = operate(operator, +firstNum, +secondNum);
+            // Save that under the first number
+            firstNum = res.toString();
+            // Display the result 
+            screen.textContent = firstNum;
+        } else {
+            // If this is the first operator we've seen, lock in the first num
+            firstNum = displayValue;
+        }
+        // Update the operator 
         operator = buttonName;
-    } else if (buttonName === "-") {
-        firstNum = displayValue;
-        operator = buttonName;
-    } else if (buttonName === "*") {
-        firstNum = displayValue;
-        operator = buttonName;
-    } else if (buttonName === "/") {
-        firstNum = displayValue;
-        operator = buttonName;
+        // Clear out display value for second number to be typed 
+        displayValue = "";
     } else if (buttonName === "=") {
-        // screen.textContent = '';
-        console.log(operate(operator, +firstNum, +secondNum));
-        screen.textContent = operate(operator, +firstNum, +secondNum);
+        secondNum = displayValue;
+        let res = operate(operator, +firstNum, +secondNum);
+        screen.textContent = res;
+        firstNum = res.toString();
+        secondNum = "";
     }
 })
 
